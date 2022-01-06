4bf6bf07621bbc42c72c8c3c44d935ccbe4ca176
Larisa Antokhina <laranto1985@gmail.com>
2022-01-06 12:12:04 +0300

    Added card component amd styled with materialize css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import './App.css';
+import 'materialize-css/dist/css/materialize.min.css'
+import 'materialize-css/dist/js/materialize.min.js'
+import M from 'materialize-css'
+
+import Card from './components/Card'
 
 function App() {
   return (
-    <h1>itechart-react-course</h1>
+    <>
+      <div className="container">
+        <h1>itechart-react-course</h1>
+        <div className='row'>
+          <Card />
+          <Card /> 
+          <Card /> 
+          <Card /> 
+        </div> 
+      </div>
+    </>
   );
 }
 
